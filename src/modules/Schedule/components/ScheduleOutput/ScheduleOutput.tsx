0a30d9093d072bf704d0e80e3cb7190a409a729a
dani quinn sexton <dani@glitter.tech>
2023-01-07 23:30:54 +0000

    Remove unused warning

--- src/modules/Schedule/components/ScheduleOutput/ScheduleOutput.tsx
+++ src/modules/Schedule/components/ScheduleOutput/ScheduleOutput.tsx
@@ -16,6 +16,8 @@ const ScheduleOutput: FunctionComponent<
 > = ({ options }) => {
   const { theme } = useParams()
 
+  console.log(`Current theme: ${theme}`)
+
   return (
     <div className={classes.ScheduleOutput}>
       <PixieTheme options={options} />
